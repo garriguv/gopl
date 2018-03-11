9bb483b22aeb419242f493a6d0ec93d7b4dd24d1
Vincent Garrigues <vincent.garrigues@gmail.com>
2018-03-11 14:18:57 +0100

    exercise7

---
+++ ch1/exercise7/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, cpErr := io.Copy(os.Stdout, resp.Body)
+		if cpErr != nil {
+			fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, cpErr)
+			os.Exit(1)
+		}
+	}
+}
