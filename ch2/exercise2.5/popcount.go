24171179c1463015da31f641d99fd447dd3f800e
Vincent Garrigues <vincent.garrigues@gmail.com>
2018-03-13 21:28:57 +0100

    Simplify algorithm
    Sinc it clears the right most bit that equals 1, we can just count the
    loops until the number is 0

--- ch2/exercise2.5/popcount.go
+++ ch2/exercise2.5/popcount.go
@@ -22,11 +22,9 @@ func PopCount(x uint64) int {
 
 func PopCountClear(x uint64) int {
 	count := 0
-	for x > 0 {
-		if x&(x-1) != x {
-			count++
-		}
+	for x != 0 {
 		x = x & (x - 1)
+		count++
 	}
 	return count
 }
